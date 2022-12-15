105e6411d9540351fca78f6005b2ac00768c2840
jcavaillac <jcavaillac@ankama.com>
2022-12-15 18:49:40 +0100

    initial commit
    gitignore
    clear
    IOC

---
+++ IOC/Assets/Scripts/Service/GameService/GameService.cs
@@ -0,0 +1,17 @@
+﻿using Attributes;
+using Service.AudioService;
+
+namespace Service
+{
+    public class GameService : IGameService
+    {
+        [DependsOnService] 
+        private IAudioService m_audioService;
+
+        [ServiceInit]
+        private void Initialize()
+        {
+            m_audioService.PlaySound(0);
+        }
+    }
+}
